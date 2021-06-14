bee0aa96861ea77077c290712ce1529b6deea66c
Leo <leo.cho1989@gmail.com>
2021-06-14 11:53:06 -0700

    second commit

---
+++ src/moving_object.js
@@ -0,0 +1,27 @@
+const Util = require("./util");
+
+function MovingObject(options) {
+  this.pos = options.pos;
+  this.vel = options.vel;
+  this.radius = options.radius;
+  this.color = options.color;
+  this.game = options.game;
+}
+
+MovingObject.prototype.collideWith = function collideWith(otherObject) {
+
+};
+
+MovingObject.prototype.draw = function draw(ctx) {
+  ctx.fillStyle = this.color;
+
+  ctx.beginPath();
+  ctx.arc(
+    this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, true
+  );
+  ctx.fill();
+};
+
+
+
+module.exports = MovingObject;
